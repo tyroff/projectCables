094ef7e44cf38d0657ceaf872a6e43007d983934
Kirill Vinokurov <tyroff@mail.ru>
2020-06-01 23:04:05 +0300

    Start

---
+++ src/org/itstep/vinokurov/domain/TNLA.java
@@ -0,0 +1,10 @@
+package org.itstep.vinokurov.domain;
+
+import java.util.Date;
+
+public class TNLA extends Entity {
+	public String cod;
+	public String name;
+	public Date dateStart;
+	public Date dateEnd;
+}
